89c61a9039c3e16b3d4e6754acd02d8025d28ffc
Jian <kongjian.work@gmail.com>
2018-07-14 23:31:14 -0500

    KJ:clean car*

--- case/controller1/stop.sh
+++ case/controller1/stop.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 
+echo "Stop and delete the Controller"
 kubectl delete -f eureka.yaml
-sleep 5 
-
 kubectl delete -f test.yaml
-sleep 5 
-
 kubectl delete -f zuul.yaml
-sleep 5 
-
 kubectl delete -f service.yaml
+
+
+echo "Stop and delete Car1"
+sudo kubectl delete deployments car1-eureka
+sudo kubectl delete deployments car1-zuul
+sudo kubectl delete deployments car1-test
+sudo kubectl delete svc car1
+
+
+echo "Stop and delete Car2"
+sudo kubectl delete deployments car2-eureka
+sudo kubectl delete deployments car2-zuul
+sudo kubectl delete deployments car2-test
+sudo kubectl delete svc car2
+
 
 
