4277de92090c52261e431a1565771f40bbd05e06
Jian <kongjian.work@gmail.com>
2018-07-16 00:17:24 -0500

    KJ:stop

--- case/controller1/stop.sh
+++ case/controller1/stop.sh
@@ -8,17 +8,28 @@ kubectl delete -f service.yaml
 
 
 echo "Stop and delete Car1"
-sudo kubectl delete deployments car1-eureka
-sudo kubectl delete deployments car1-zuul
-sudo kubectl delete deployments car1-test
-sudo kubectl delete svc car1
+sudo kubectl delete deployments car1-0-eureka
+sudo kubectl delete deployments car1-0-zuul
+sudo kubectl delete deployments car1-0-test
+sudo kubectl delete svc car1-0
+
+sudo kubectl delete deployments car1-1-eureka
+sudo kubectl delete deployments car1-1-zuul
+sudo kubectl delete deployments car1-1-test
+sudo kubectl delete svc car1-1
+
+
 
 
 echo "Stop and delete Car2"
-sudo kubectl delete deployments car2-eureka
-sudo kubectl delete deployments car2-zuul
-sudo kubectl delete deployments car2-test
-sudo kubectl delete svc car2
+sudo kubectl delete deployments car2-0-eureka
+sudo kubectl delete deployments car2-0-zuul
+sudo kubectl delete deployments car2-0-test
+sudo kubectl delete svc car2-0
 
+sudo kubectl delete deployments car1-1-eureka
+sudo kubectl delete deployments car1-1-zuul
+sudo kubectl delete deployments car1-1-test
+sudo kubectl delete svc car2-1
 
 
