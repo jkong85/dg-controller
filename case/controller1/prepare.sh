bb36f1ddc49a379f3f7ba3b978d1a76d56e3e5f6
Jian <kongjian.work@gmail.com>
2018-07-16 11:27:24 -0500

    KJ:add java_opts in eureka pom file add prepare file to git pull images

---
+++ case/controller1/prepare.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#set -x
+
+sudo docker pull jkong85/dg-controller-eureka:0.1
+
+sudo docker pull jkong85/dg-controller-zuul:0.1
+
+sudo docker pull jkong85/dg-controller-test:0.1
+
+sudo docker pull jkong85/dg-imo-eureka:0.1
+
+sudo docker pull jkong85/dg-imo-zuul:0.1
+
+sudo docker pull jkong85/dg-imo-speed:0.1
+
+sudo docker pull jkong85/dg-imo-oil:0.1
+
+sudo docker pull jkong85/dg-imo-location:0.1
