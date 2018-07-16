95b6ff7155ee1a011edcc0dc91620993502c509b
Jian <kongjian.work@gmail.com>
2018-07-16 00:19:31 -0500

    KJ:stop

--- case/controller1/stop.sh
+++ case/controller1/stop.sh
@@ -11,11 +11,17 @@ echo "Stop and delete Car1"
 sudo kubectl delete deployments car1-0-eureka
 sudo kubectl delete deployments car1-0-zuul
 sudo kubectl delete deployments car1-0-test
+sudo kubectl delete deployments car1-0-speed
+sudo kubectl delete deployments car1-0-oil
+sudo kubectl delete deployments car1-0-location
 sudo kubectl delete svc car1-0
 
 sudo kubectl delete deployments car1-1-eureka
 sudo kubectl delete deployments car1-1-zuul
 sudo kubectl delete deployments car1-1-test
+sudo kubectl delete deployments car1-1-speed
+sudo kubectl delete deployments car1-1-oil
+sudo kubectl delete deployments car1-1-location
 sudo kubectl delete svc car1-1
 
 
@@ -25,11 +31,17 @@ echo "Stop and delete Car2"
 sudo kubectl delete deployments car2-0-eureka
 sudo kubectl delete deployments car2-0-zuul
 sudo kubectl delete deployments car2-0-test
+sudo kubectl delete deployments car2-0-speed
+sudo kubectl delete deployments car2-0-oil
+sudo kubectl delete deployments car2-0-location
 sudo kubectl delete svc car2-0
 
-sudo kubectl delete deployments car1-1-eureka
-sudo kubectl delete deployments car1-1-zuul
-sudo kubectl delete deployments car1-1-test
+sudo kubectl delete deployments car2-1-eureka
+sudo kubectl delete deployments car2-1-zuul
+sudo kubectl delete deployments car2-1-test
+sudo kubectl delete deployments car2-1-speed
+sudo kubectl delete deployments car2-1-oil
+sudo kubectl delete deployments car2-1-location
 sudo kubectl delete svc car2-1
 
 
