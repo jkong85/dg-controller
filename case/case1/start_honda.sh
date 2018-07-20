02c28bf2b7905d91d92edd2c2517f22446fc94f0
Jian <kongjian.work@gmail.com>
2018-07-20 15:57:58 -0500

    KJ:car emulator

--- case/case1/start_honda.sh
+++ case/case1/start_honda.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-set -x
 echo "start car honda1 with location and speed services ... "
-java -jar car-emulator/target/car-emulator-0.0.1-SNAPSHOT.jar honda1 honda
+java -jar ../../car-emulator/target/car-emulator-0.0.1-SNAPSHOT.jar honda1 honda
--- case/case1/start_toyota.sh
+++ case/case1/start_toyota.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-set -x
 echo "start car honda1 with location and speed services ... "
-java -jar car-emulator/target/car-emulator-0.0.1-SNAPSHOT.jar toyota1 toyota
+java -jar ../../car-emulator/target/car-emulator-0.0.1-SNAPSHOT.jar honda1 honda
