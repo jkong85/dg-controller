a425071141bfa9559739e2ef747ce197ed2b6506
Jian <kongjian.work@gmail.com>
2018-07-20 09:33:32 -0500

    KJ:rm tmp file

--- clean_docker_images.sh
+++ clean_docker_images.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -x
 echo "please first save the images info to file "all""
-cat all | awk '{print $3}' >> id
+cat $1 | awk '{print $3}' >> id
 
 result="$(cat id)"
 
@@ -9,5 +9,8 @@ for line in $result
 do
     sudo docker rmi -f ${line}
 done
+
+rm $1
+rm id
 
 
