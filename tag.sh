59d98f38949dc2921d00906c3bfd660ca56738ba
Jian <kongjian.work@gmail.com>
2018-07-20 09:55:08 -0500

    KJ:car emulator shell

--- tag.sh
+++ tag.sh
@@ -6,6 +6,8 @@ if [ ! "$1" ]; then
     echo "To tag an image, sudo ./tag.sh name tag_id"
     exit
 fi
+
+
 sudo docker tag $2 jkong85/dg-controller-$1:0.1
 sudo docker tag $2 jkong85/dg-imo-$1:0.1
 
