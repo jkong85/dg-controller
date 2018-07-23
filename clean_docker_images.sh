53a70b6783e47f28bfba6f17e77e36b804913ef7
Jian <kongjian.work@gmail.com>
2018-07-23 10:27:51 -0500

    KJ:

--- clean_docker_images.sh
+++ clean_docker_images.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 set -x
+
+if [ ! "$1" ]; then
+    echo "please specify the images name"
+    exit
+fi
+
+sudo docker images | grep $1 | awk '{print $3}' | xargs sudo docker rmi -f
+
+exit 
+
 echo "please first save the images info to file "all""
 cat $1 | awk '{print $3}' >> id
 
