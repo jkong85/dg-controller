932f5cc6557915dcbba7d5d89b47f2fc1dde98c5
Jian <kongjian.work@gmail.com>
2018-07-23 11:39:55 -0500

    KJ:change oil to gas

--- case/case1/start_toyota.sh
+++ case/case1/start_toyota.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo "start car honda1 with location and speed services ... "
+echo "start car toyota with location and gas services ... "
 java -jar ../../car-emulator/target/car-emulator-0.0.1-SNAPSHOT.jar toyota1 toyota
