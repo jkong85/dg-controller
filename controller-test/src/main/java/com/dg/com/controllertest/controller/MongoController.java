f87b205f5314d9f1ad3dc6c4b9d8ff18b3cab88d
Jian <kongjian.work@gmail.com>
2018-08-03 14:15:03 -0500

    KJ:test mongo

--- controller-test/src/main/java/com/dg/com/controllertest/controller/MongoController.java
+++ controller-test/src/main/java/com/dg/com/controllertest/controller/MongoController.java
@@ -4,7 +4,10 @@ import com.mongodb.DB;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
 
 /**
  * Created by jkong on 8/2/18.
@@ -21,5 +24,26 @@ public class MongoController {
         DB db = mongoTemplate.getDb();
 
         return db.getName();
+    }
+    @RequestMapping("/speed")
+    public String speed(@RequestParam String time,
+                        @RequestParam String log ){
+        mongoTemplate.insert(new logData(time, log));
+
+        return null;
+    }
+    @RequestMapping("/speedlog")
+    public String speedlog(){
+        Set<String> result  = mongoTemplate.getCollectionNames();
+        return result.toString();
+    }
+
+    private class logData{
+        String timeStamp;
+        String value;
+        logData(String time, String value){
+            this.timeStamp = time;
+            this.value = value;
+        }
     }
 }
