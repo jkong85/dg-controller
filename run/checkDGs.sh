d6c2d0b3488d236cfcb3dcffa207657e1c3c5017
Jian <kongjian.work@gmail.com>
2018-08-09 10:10:42 -0500

    KJ:

--- run/checkDGs.sh
+++ run/checkDGs.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-
+echo "======================================================"
+echo "Get Pods Info"
 sudo kubectl get pods -o wide
 
+echo "======================================================"
+echo "Get SVC Info"
 sudo kubectl get svc
 
