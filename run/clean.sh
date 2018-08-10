db2a35f0a55ba26877f7f143837fe2d0b764a906
Jian <kongjian.work@gmail.com>
2018-08-10 09:45:50 -0500

    KJ:

--- run/clean.sh
+++ run/clean.sh
@@ -4,15 +4,16 @@
 
 clear
 
-echo " rm DGs"
+echo " Remove the DGs left if existed"
 ./rmDGs.sh dg-core-honda-1 dg-edge1-honda-2 dg-edge2-honda-3
-
-echo "stop controller"
-
-sudo ./stop.sh
 
 sudo kubectl delete svc honda1-core
 sudo kubectl delete svc honda1-edge1
+sudo kubectl delete svc honda1-edge2
+
+
+echo "<<<<<Stop all controller Components>>>>>"
+sudo ./stop.sh
 
 
 ./checkDGs.sh
